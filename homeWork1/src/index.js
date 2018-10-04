583d66680388e29664f5f303c5b887e98a9b5aa3
GaponVasa <gapon.vasa@ukr.net>
2018-10-04 11:13:56 +0300

    change index.js and updated webpack-dev-server

--- homeWork1/src/index.js
+++ homeWork1/src/index.js
@@ -6,23 +6,15 @@ import {AddUserInfo} from './components/AddUserInfo.js';
  
 const mountNode = document.getElementById('app');
 
-class AddUsersCard extends React.Component{
-	constructor(props){
-		super(props);
-	}
-
-	createUsersArr(){
-		const {arr} = this.props;
-		return arr.map((el, ind) =>{
+const AddUsersCard = (props)=>{
+	const {arr} = props;
+	return(
+		arr.map((el, ind) =>{
 			el.id = ind + 1;
 			return(<AddUserInfo key={ind} aboutUser={el}/>) 
-		});
-	}
-
-	render(){
-		return this.createUsersArr();
-	}
-};
+		})
+	)
+}
 
 
 axios.get('https://randomuser.me/api/?results=5&inc=gender,name,dob,email,phone,location,picture')
