17358aa106148e7a86ef6d48c7242ef1279b3762
GaponVasa <gapon.vasa@ukr.net>
2018-10-11 10:14:31 +0300

    add button Delete to cart list admin and user

---
+++ class-work2/myapp/src/components/shared/input-group-with-button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from './button';
+
+export const InputGroup = (props)=>{
+  const {onclick, textButton, value, onchange, placeholder} = props;
+  return(
+    <div className="input-group mb-3">
+      <input 
+        type="text" 
+        className="form-control" 
+        placeholder={placeholder} 
+        value={value}
+        onChange={onchange}
+        aria-describedby="button-addon2"
+      />
+      <div className="input-group-append">
+        <Button
+          cssClass={"btn btn-outline-secondary"}
+          onclick={onclick}
+          textButton={textButton}
+        />
+      </div>
+    </div>
+  )
+}
