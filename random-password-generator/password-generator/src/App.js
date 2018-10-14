bd104f676307e089e7d41d01a8a3d5e8b85f8041
GaponVasa <gapon.vasa@ukr.net>
2018-10-14 21:34:10 +0300

    add generator.js and result page

--- random-password-generator/password-generator/src/App.js
+++ random-password-generator/password-generator/src/App.js
@@ -1,44 +1,24 @@
 import React, { Component } from 'react';
 
+import DataEntry from './pages/data-entry.js'
+import {symbolArr} from './pages/data-entry-array.js';
+import {password_generator} from './generate.js';
+
 class App extends Component {
+
+  generatePassword = (len)=>{
+    console.log(typeof password_generator);
+    console.log(password_generator(len));
+  }
+
   render() {
     return (
       <div className="container-fluid d-flex justify-content-center">
         <div className="col-6 my-3">
-          <form className="d-flex flex-column justify-content-center">
-            <div className="form-group">
-              <h1>Random password generator.</h1>
-            </div>
-            <div className="form-group">
-              <input type="text" className="form-control" placeholder="Enter length password."/>
-              <small id="emailHelp" className="form-text text-muted">Password length in characters.</small>
-            </div>
-            <div className="form-group">
-              <input type="checkbox" className="" id="exampleCheck1"/>
-              <label className="form-check-label" htmlFor="exampleCheck1">A-Za-z0-9</label>
-            </div>
-            <div className="form-group">
-              <input type="checkbox" className="" id="exampleCheck2"/>
-              <label className="form-check-label" htmlFor="exampleCheck2"></label>
-              {/*~ ! @ # $ % ^ & * ( ) _ + ` - = { } [ ] : ; < > . / \ */}
-            </div>
-            {/* <div className="form-group">
-              <input type="checkbox" className="" id="exampleCheck3"/>
-              <label className="form-check-label" htmlFor="exampleCheck3"></label>
-            </div>
-            <div className="form-group">
-              <input type="checkbox" className="" id="exampleCheck4"/>
-              <label className="form-check-label" htmlFor="exampleCheck4">()!@#$%^&*_+</label>
-            </div> */}
-            
-            <button type="submit" className="col-4 btn btn-primary ">Generate password</button>
-          </form>
-          <div className="input-group my-3">
-            <input type="text" className="form-control" aria-describedby="button-addon2"/>
-            <div className="input-group-append">
-              <button className="btn btn-outline-secondary" type="button" id="button-addon2">Copy</button>
-            </div>
-          </div>
+          <DataEntry 
+            symbolArr={symbolArr}
+            generatePassword={this.generatePassword}
+          />
         </div>
       </div>
     );
